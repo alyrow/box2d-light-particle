4109aef66750d17276eb1a3223d883063602927e
MCXIV <mcxiv14@gmail.com>
2020-11-05 13:27:04 +0530

    Removed T,

--- src/test/java/com/alyrow/gdx/particle/physics/ImageColourTest.java
+++ src/test/java/com/alyrow/gdx/particle/physics/ImageColourTest.java
@@ -14,17 +14,17 @@ public class ImageColourTest {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedImage i = ImageIO.read(new File("D:\\i.png"));
-        Color[][] c = new Color[i.getWidth()][i.getHeight()];
-        for(int j = 0; j < i.getWidth(); j++) {
-            for(int k = 0; k < i.getHeight(); k++) {
-                java.awt.Color f = new java.awt.Color(i.getRGB(j, k));
-                c[j][k] = new Color(f.getRed(), f.getGreen(), f.getBlue(), 255);
-            }
-        }
+//        BufferedImage i = ImageIO.read(new File("D:\\i.png"));
+//        Color[][] c = new Color[i.getWidth()][i.getHeight()];
+//        for(int j = 0; j < i.getWidth(); j++) {
+//            for(int k = 0; k < i.getHeight(); k++) {
+//                java.awt.Color f = new java.awt.Color(i.getRGB(j, k));
+//                c[j][k] = new Color(f.getRed(), f.getGreen(), f.getBlue(), 255);
+//            }
+//        }
         new Tester()
                 .forModifier(() -> new RandomPositionRectangle(Gdx.graphics.getWidth(), Gdx.graphics.getWidth()))
-                .forForce(() -> new ImageColour(c/*Gdx.files.absolute("D:\\i.png")*/))
+                .forForce(() -> new ImageColour(/*c*/Gdx.files.absolute("D:\\i.png")))
                 .setPc(1000).Test();
 
     }
