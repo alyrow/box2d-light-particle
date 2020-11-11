5fb6e372b8e4242e5766aefe48dd6e7036ef12bf
alyrow <58048578+alyrow@users.noreply.github.com>
2020-11-11 14:59:53 +0100

    MappedForce LEVEL 2
    Code cleanup is needed

--- src/main/java/com/alyrow/gdx/particle/physics/Fan.java
+++ src/main/java/com/alyrow/gdx/particle/physics/Fan.java
@@ -13,14 +13,11 @@ public class Fan extends PhysicForce {
         this.speed = speed/100;
     }
 
-    private Vector2 cache = Vector2.Zero;
+    private Vector2 cache = new Vector2(0, 0);
 
     @Override
     public Vector2 getForce(PhysicParticle particle) {
-        cache.x = particle.y - center.y;
-        cache.y = center.x - particle.x;
-
-        return cache.scl(speed);
+        return cache.set(particle.y - center.y, center.x - particle.x).scl(speed);
     }
 
 }
