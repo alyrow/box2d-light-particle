7b4eda1fcaf9172e7327ce4879cce16845f148c2
MCXIV <mcxiv14@gmail.com>
2020-11-12 23:18:49 +0530

    Image Serialization
    Well, lemme sum up the steps...
    * Implement ImageSerializable
    * Implement methods
    ** In getter create a MetaDataPacket and write in the values you want to be saved.
    ** In setter get those values and store them in their respective variables. Make sure you do that in the same order!
    * Then just pack the data 😁

---
+++ src/main/java/com/alyrow/gdx/particle/utils/ImageSerializable.java
@@ -0,0 +1,9 @@
+package com.alyrow.gdx.particle.utils;
+
+public interface ImageSerializable {
+
+    MetaDataPacket getByteData();
+
+    void setByteData(MetaDataPacket packet);
+
+}
---
+++ src/main/java/com/alyrow/gdx/particle/utils/JSONSerializable.java
@@ -0,0 +1,9 @@
+package com.alyrow.gdx.particle.utils;
+
+public interface JSONSerializable {
+
+    String getRows();
+
+    void setRows(String[] rows);
+
+}
