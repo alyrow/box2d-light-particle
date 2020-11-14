b3b9f903663acc33ae7a3e37230212d3bf7b2092
MCXIV <mcxiv14@gmail.com>
2020-11-14 16:46:30 +0530

    Update MappedFanVersusFan.java

--- src/test/java/com/alyrow/gdx/particle/physics/other/MappedFanVersusFan.java
+++ src/test/java/com/alyrow/gdx/particle/physics/other/MappedFanVersusFan.java
@@ -1,7 +1,7 @@
 package com.alyrow.gdx.particle.physics.other;
 
 import com.alyrow.gdx.particle.physics.Fan;
-import com.alyrow.gdx.particle.physics.MappedFan;
+import com.alyrow.gdx.particle.physics.powerups.MappedForce;
 import com.alyrow.gdx.particle.tester.Tester;
 import com.badlogic.gdx.Gdx;
 
@@ -10,8 +10,8 @@ public class MappedFanVersusFan {
     public static void main(String[] args) {
 
         new Tester()
-                .forForce(() -> new Fan /**/ (Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f, 20))
-                //.forForce(() -> new MappedFan(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f, 0.2f))
+                .forForce(() -> new Fan (Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f, 20))
+                .forForce(game -> new MappedForce(new Fan /**/(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f, 20), true, game.getCamera()))
                 .Test();
 
     }
