bfb7fda39d7e5a834baa9ea11d931608b96d3054
alyrow <58048578+alyrow@users.noreply.github.com>
2020-12-06 19:42:09 +0100

    Half json serialization of ParticleLife

--- src/main/java/com/alyrow/gdx/particle/rules/ParticleLife.java
+++ src/main/java/com/alyrow/gdx/particle/rules/ParticleLife.java
@@ -1,10 +1,14 @@
 package com.alyrow.gdx.particle.rules;
 
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
 /**
  * @author alyrow
  * Set the life of particles
  */
-public class ParticleLife {
+public class ParticleLife implements Json.Serializable {
     public float life; //in seconds
     public boolean outer = false; //If false --> Survive `life` seconds then die
 
@@ -38,5 +42,27 @@ public class ParticleLife {
 
     public void setLife(float life) {
         this.life = life;
+    }
+
+
+    @Override
+    public void write(Json json) {
+        boolean random = this instanceof ParticleLifeRandom;
+        json.writeValue("random", random);
+        json.writeValue("outer", outer);
+        Array<Float> lifes = new Array<>();
+        if (random) {
+            lifes.add(((ParticleLifeRandom) this).life_min);
+            lifes.add(((ParticleLifeRandom) this).life_max);
+        } else lifes.add(life);
+        json.writeValue("life", lifes);
+    }
+
+    @Override
+    public void read(Json json, JsonValue jsonData) {
+
+        //center = new Vector2(jsonData.getFloat("center x"), jsonData.getFloat("center y"));
+        //speed = jsonData.getFloat("speed");
+
     }
 }
